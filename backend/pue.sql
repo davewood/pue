cfe7498bfc34df7c4ac25037a270d4a1f1089ece
David Schmidt <mail@davidschmidt.at>
2018-09-13 17:06:00 +0200

    add demo data to sql

--- backend/pue.sql
+++ backend/pue.sql
@@ -2,3 +2,8 @@ CREATE TABLE usr (
   usr_id integer PRIMARY KEY,
   name test NOT NULL
 );
+
+INSERT INTO usr VALUES (1,'david');
+INSERT INTO usr VALUES (2,'elias');
+INSERT INTO usr VALUES (3,'juergen');
+INSERT INTO usr VALUES (4,'helena');
